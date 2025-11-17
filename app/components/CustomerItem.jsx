edf7e247e4266b3f0bfbb796ed4cc538bc223446
farzanacoder <farzanaashif10@gmail.com>
2025-11-17 10:27:30 +0600

    done

---
+++ app/components/CustomerItem.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { IoMdStar } from 'react-icons/io'
+import CommonHead from './CommonHead'
+import { MdVerified } from 'react-icons/md'
+
+export default function CustomerItem() {
+  return (
+    <div className='rounded-lg shadow-lg flex flex-col items-start p-10 w-[30%] mx-auto'>
+        <p className='text-lg text-[#616161] mb-6'>“Arome supplements have been a game-changer for me! I feel more energized and healthier than ever. Highly recommend!”</p>
+        <div className='flex items-center justify-center gap-1 text-[#5d3754]'>
+                    <IoMdStar />
+                    <IoMdStar />
+                    <IoMdStar />
+                    <IoMdStar />
+                    <IoMdStar />
+         </div>
+
+         <CommonHead Heading="Olivor Brown" className="text-[25px] mt-6 mb-1.5"/>
+         <div className="flex gap-2">
+            <MdVerified className='text-xl text-[#32c2ab]' />
+         <p>Verified Buyer</p>
+         </div>
+    </div>
+  )
+}
+
+
+
+// Emma Richardson
+//  Verified Buyer
+// “Arome supplements have been a game-changer for me! I feel more energized and healthier than ever. Highly recommend!”
+
+// "Excellent service and top-notch supplements. The loyalty rewards is a great bonus. Will definitely keep ordering!"
+// Sophia Martinez
+//  Verified Buyer
+// “Arome supplements have been a game-changer for me! I feel more energized and healthier than ever. Highly recommend!”
+// Emma Richardson
+//  Verified Buyer
+// “Arome supplements have been a game-changer for me! I feel more energized and healthier than ever. Highly recommend!”
--- app/layouts/Customers.jsx
+++ app/layouts/Customers.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import CommonHead from '../components/CommonHead'
+import CustomerItem from '../components/CustomerItem'
 
 export default function Customers() {
   return (
-    <section className="my-20">
-            <CommonHead className='text-center w-[30%] mx-auto' Heading="Trusted by Thousands of Satisfied Customers."/>
-        <div className="container">
-            
+    <section className="py-20 bg-[#f8f8f8]">
+            <CommonHead className='text-center w-[25%] mx-auto mb-10' Heading="Trusted by Thousands of Satisfied Customers."/>
+        <div className="container flex items-center justify-between gap-10 flex-wrap">
+            <CustomerItem/>
+            <CustomerItem/>
+            <CustomerItem/>
         </div>
     </section>
   )
